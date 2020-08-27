dc0ed5ba47112e5d41b8a8b4f3d680c8e59b075b
pradyumansingh989 <66694735+pradyumansingh989@users.noreply.github.com>
2020-08-27 15:11:57 +0530

    Create Factorial.py

---
+++ Factorial.py
@@ -0,0 +1,29 @@
+# way-1
+ 
+import math
+fact = int(input())
+print(math.factorial(int(fact)))
+ 
+# way-2
+ 
+# n = input("Enter a number: ")
+# factorial = 1
+# if int(n) >= 1:
+# for i in range (1,int(n)+1):
+#    factorial = factorial * i
+# print("Factorail of ",n , " is : ",factorial)
+ 
+# way-3
+ 
+# fact = int(input())
+ 
+ 
+# def factorial(n):
+#     if(n == 1):
+#         return n
+#     elif(n < 1):
+#         return ("koi factorial possible nhi hai pagal")
+#     else:
+#         return n*factorial(n-1)
+ 
+# print (factorial(int(fact)))
